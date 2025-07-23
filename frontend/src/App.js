979ac79a004d0c65b21626230afcb7aaba624ebf
siri29 <aishugowda308@gmail.com>
2025-07-23 12:19:09 +0530

    Added Bootstrap UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Container, Button } from "react-bootstrap";
 function App() {
   return (
     <Container className="mt-5">
-      <h1 className="text-left">Knowledge Base Platform</h1>
+      <h1 className="text-centre">Knowledge Base Platform</h1>
       <Button variant="primary">Test Bootstrap Button</Button>
     </Container>
   );
